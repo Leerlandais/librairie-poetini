8363260663a6ff9fbf35883cb4759bfef044f35d
leerlandais <leerlandais.com>
2025-04-10 17:43:10 +0200

    improved button glamour

--- public/scripts/buttonFlash.js
+++ public/scripts/buttonFlash.js
@@ -1,10 +1,28 @@
-console.log("button flash active")
 const librelButton = document.getElementById("librelButton");
-console.log(librelButton);
 
-let buttonFlash = setInterval(() => {
-    librelButton.classList.add ("flashButton");
-    setTimeout(() => {
-        librelButton.classList.remove("flashButton");
-    }, 1500);
-}, 3000);
+let buttonFlash = startFlashing();
+
+function startFlashing() {
+    return setInterval(() => {
+        librelButton.classList.add("flashButton");
+        setTimeout(() => {
+            librelButton.classList.remove("flashButton");
+        }, 3500);
+    }, 6000);
+}
+
+let isPaused = false;
+
+librelButton.addEventListener("mouseover", function(event) {
+    if (!isPaused) {
+        clearInterval(buttonFlash);
+        isPaused = true;
+    }
+});
+
+librelButton.addEventListener("mouseout", function(event) {
+    if (isPaused) {
+        buttonFlash = startFlashing();
+        isPaused = false;
+    }
+});
