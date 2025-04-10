9becfeacf9a9cbf2891a95855b9acbd8b59462a3
leerlandais <leerlandais@gmail.com>
2025-04-10 17:28:26 +0200

    glammed up

--- public/scripts/buttonFlash.js
+++ public/scripts/buttonFlash.js
@@ -1,3 +1,10 @@
 console.log("button flash active")
 const librelButton = document.getElementById("librelButton");
 console.log(librelButton);
+
+let buttonFlash = setInterval(() => {
+    librelButton.classList.add ("flashButton");
+    setTimeout(() => {
+        librelButton.classList.remove("flashButton");
+    }, 1500);
+}, 3000);
